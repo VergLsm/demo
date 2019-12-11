ce4adb50f42428a72ded28f44651282ac4c15746
VergLsm <vision.lsm.2012@gmail.com>
2019-12-11 10:05:43 +0000

    Jump to static web;

--- src/main/java/com/example/demo/controller/FreeMarkerCtrl.java
+++ src/main/java/com/example/demo/controller/FreeMarkerCtrl.java
@@ -1,11 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class FreeMarkerCtrl {
@@ -16,10 +12,9 @@ public class FreeMarkerCtrl {
     //     return file;
     // }
 
-    @ResponseBody
     @RequestMapping(value = "/")
     public String root() {
-        return "Thanks For Posting!!!!";
+        return "redirect:webpage/web/guaranteeQuery.html";
     }
 
 }
