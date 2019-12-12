e34889d6dc5f7d58d8feb222ca363cc718087c90
VergLsm <vision.lsm.2012@gmail.com>
2019-12-12 16:53:30 +0000

    Added captcha;

--- src/main/java/com/example/demo/controller/VerificationCodeController.java
+++ src/main/java/com/example/demo/controller/VerificationCodeController.java
@@ -1,10 +1,21 @@
 package com.example.demo.controller;
 
+import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.google.code.kaptcha.Constants;
+import com.google.code.kaptcha.Producer;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -14,19 +25,38 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @RequestMapping("/verificationCodeController")
 public class VerificationCodeController {
 
+    @Autowired
+    private Producer captchaProducer;
+
+    @ResponseBody
     @RequestMapping("/imgmaker")
-    public Object imgmaker() {
-        return "";
+    public void imgmaker(HttpSession httpSession, HttpServletResponse response) throws IOException {
+        //生成验证码
+        String capText = captchaProducer.createText();
+        httpSession.setAttribute(Constants.KAPTCHA_SESSION_KEY, capText);
+        //向客户端写出
+        BufferedImage bi = captchaProducer.createImage(capText);
+        ServletOutputStream out = response.getOutputStream();
+        ImageIO.write(bi, "jpg", out);
+        try {
+            out.flush();
+        } finally {
+            out.close();
+        }
     }
 
     @ResponseBody
     @RequestMapping("/getValidateCode")
-    public Map<String, Object> getValidateCode(@RequestParam String verificationCode) {
+    public Map<String, Object> getValidateCode(HttpSession httpSession, @RequestParam String verificationCode) {
+        String capText = (String) httpSession.getAttribute(Constants.KAPTCHA_SESSION_KEY);
+        if (capText == null || capText.equals("")) {
+            return null;
+        }
         // {"success":true,"msg":"操作成功","obj":"Y","status":null,"info":"所填内容在系统中已存在,请检查","attributes":null,"next":null}
         Map<String, Object> map = new HashMap<String, Object>();
         map.put("success", true);
         map.put("msg", "操作成功");
-        if (verificationCode.equals("123")) {
+        if (verificationCode.toUpperCase().equals(capText)) {
             map.put("obj", "Y");
         } else {
             map.put("obj", "N");
